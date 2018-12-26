66e250a7e6794696478e22ac037cdcea43447bfa
Nutan Nepal <nnutannep@gmail.com>
2018-12-25 19:27:22 -0500

    enter

--- Haskell/cw_sum_factors.hs
+++ Haskell/cw_sum_factors.hs
@@ -2,21 +2,14 @@ import Data.List
 
 main :: IO()
 main = do
-    let inp = [15,21,24,30,45]
-    print (sumOfDivided inp) 
+    let inp = [400, 500, 600, 110, 126, 107, 158, 204, 159201, 50000000001]
+    print (sumOfDivided inp)
 
 sumOfDivided :: [Integer] -> [(Integer, Integer)]
-sumOfDivided inp = a where
-    ys = sort [ n | x <- inp, n <- [2..x-1], x `rem` n == 0 ]                   --factors
-    xs = diff (nub ys) (nub [ n | n <- ys, z <- ys, n > z, n `rem` z == 0 ])    --prime factors
-    a = [ (p, r) | p <- xs, r <- inp, r `rem` p == 0 ]                          --pairs of factors and input numbers
-
-
-rmdups2 :: [(Integer, Integer)] -> [(Integer, Integer)]
-rmdups2 = map head . group . sort
-diff :: [Integer] -> [Integer] -> [Integer]
-diff x y = [ z | z <- x, z `notElem` y]
-diff2 :: [(Integer, Integer)] -> [(Integer, Integer)] -> [(Integer, Integer)]
-diff2 x y = [ z | z <- x, z `notElem` y]
-add :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
-add x y = (fst x, snd x + snd y)
+sumOfDivided inp = out where
+    zss = [ [n, m] | x <- inp, n <- [1,2..((floor.sqrt.abs.fromIntegral) x) + 1],
+        x `rem` n == 0, m <- [quot (abs x) n]] --get all factors
+    ys = (tail.sort.nub.concat) zss --remove 1 and duplicates and sort
+    xs = ys \\ [ n | n <- ys, z <- ys, n > z, n `rem` z == 0 ]  --prime factors
+    --for each prime factor, calculate the sum
+    out = [ (p, q) | p <- xs, q <- [sum [if x `rem` p == 0 then x else 0 | x <- inp]]]
